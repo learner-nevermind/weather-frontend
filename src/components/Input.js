2a8050ab4917c0a6234d654fe4728eccc3fc17bf
learner-nevermind <superdevv0292@gmail.com>
2023-10-30 11:01:42 -0400

    feat: updated input

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,7 +1,7 @@
 function Input(props) {
   return <input
     placeholder={props.placeholder}
-    className="w-full rounded-2xl border border-slate-200 px-2 py-1"
+    className="w-full rounded-2xl border border-slate-200 px-3 py-2"
     {...props}
   />
 }

f3110a52be332da284d1c4d1dfab710349b754c3
learner-nevermind <superdevv0292@gmail.com>
2023-10-30 11:01:28 -0400

    feat: updated appbar

--- src/components/layout/Appbar.js
+++ src/components/layout/Appbar.js
@@ -7,9 +7,9 @@ import { appRoutes } from "../../constants";
 function Appbar() {
   const navigate = useNavigate();
 
-  const onGoToProfile = () => {
-    navigate(appRoutes.profile);
-  };
+  // const onGoToProfile = () => {
+  //   navigate(appRoutes.profile);
+  // };
 
   const onSignOut = () => {
     localStorage.clear();
@@ -55,7 +55,7 @@ function Appbar() {
                     leaveTo="transform opacity-0 scale-95"
                   >
                     <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <Menu.Item>
+                      {/* <Menu.Item>
                         {({ active }) => (
                           <div
                             className={classNames(
@@ -67,7 +67,7 @@ function Appbar() {
                             Your Profile
                           </div>
                         )}
-                      </Menu.Item>
+                      </Menu.Item> */}
                       <Menu.Item>
                         {({ active }) => (
                           <div
